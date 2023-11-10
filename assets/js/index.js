29e53d5bbb22ee49b5699acea0494bed1d715a9d
Alfredo GM <alfredogonzalezm265@gmail.com>
2023-11-10 14:28:56 -0600

    update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,7 @@
 console.log("Entro a index");
 
+const card = document.getElementById("card");
+
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
@@ -9,27 +11,90 @@ document.addEventListener("DOMContentLoaded", ()=>{
   fetchData(random)
 });
 
+const fetchData = async(id) => {
+  try {
+    console.log(id)
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+
+      
+        const img =  src=`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`;
+        const nombre =  data.name;
+        const experiencia =  data.base_experience;
+        const hp =  data.stats[0].base_stat;
+        const ataque =  data.stats[1].base_stat;
+        const defensa =  data.stats[2].base_stat;
+        const especial =  data.stats[3].base_stat;
+    
+
+      const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ['Ataque', 'Defensa', 'Especial'],
+    datasets: [{
+      label: 'Atributos Pokemon',
+      data: [ataque,defensa,especial],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+
+
+    
+    card.innerHTML = `
+    <div class="card-header">
+    <img src="assets/images/bg-pattern-card.svg" alt="imagen header card" class="card-header">
+<div class="card-body">
+    <img src="${img}" alt="imagen de pokedex" class="card-body-img">
+</div>
+<div class="card-body">
+<h1 class="card-body-title">
+    ${nombre}
+    <span>${hp}</span>
+</h1>
+<p class="card-body-text">${experiencia}</p>
+</div>
+<div class="card-footer text-muted">
+    <div class="card-footer-social">
+        <h3>${ataque + 'k'}</h3>
+        <p>Ataque</p>
+    </div>
+    <div class="card-footer-social">
+        <h3>${especial + 'k'}</h3>
+        <p>Ataque Especial</p>
+    </div>
+    <div class="card-footer-social">
+        <h3>${defensa + 'k'}</h3>
+        <p>Defensa</p>
+    </div>
+</div>
+</div>
+    `;
+
+    })
+
+  }catch (error) {
+    console.log(error);
+  }
+}
 
 
 
 
-// const ctx = document.getElementById('myChart');
 
-// new Chart(ctx, {
-//   type: 'doughnut',
-//   data: {
-//     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-//     datasets: [{
-//       label: '# of Votes',
-//       data: [12, 19, 3, 5, 2, 3],
-//       borderWidth: 1
-//     }]
-//   },
-//   options: {
-//     scales: {
-//       y: {
-//         beginAtZero: true
-//       }
-//     }
-//   }
-// });
+
+
+
